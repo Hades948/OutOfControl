b63411738ef28be37c12249654e55d0a23e64531
Hades948 <hades948@gmail.com>
2020-07-10 16:49:53 -0400

    Initial commit

---
+++ Out Of Control/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private const float DEADZONE = 0.1f;
+    public float speed;
+    void Start() {
+        
+    }
+
+    void FixedUpdate() {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+
+        if (hAxis > DEADZONE) {
+            // Move right.
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        } else if (hAxis < -DEADZONE) {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+    }
+}
