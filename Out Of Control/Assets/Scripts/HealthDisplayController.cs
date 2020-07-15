ba4294e576d1ce6a74c0d41b78ca33a92a3f2d9e
Hades948 <hades948@gmail.com>
2020-07-15 19:48:52 -0400

    Finish health display

---
+++ Out Of Control/Assets/Scripts/HealthDisplayController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplayController : MonoBehaviour {
+    public HealthScriptableObject health;
+    private Color opaque = new Color(1, 1, 1, 1);
+    private Color transparent = new Color(1, 1, 1, 0);
+    private List<Image> images;
+
+    void Start() {
+        images = new List<Image>();
+
+        foreach (Transform t in transform) {
+            images.Add(t.gameObject.GetComponent<Image>());
+        }
+    }
+
+    void Update() {
+        for (int i = 0; i < images.Count; i++) {
+            images[i].color = (health.value >= i+1 ? opaque : transparent);
+        }
+    }
+}
