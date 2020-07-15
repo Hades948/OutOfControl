730ce291264372bff78f361ea472ae2500f50400
Hades948 <hades948@gmail.com>
2020-07-15 14:00:43 -0400

    Update UI Finish controls display Start work on health display

---
+++ Out Of Control/Assets/Scripts/ControlLabelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlLabelController : MonoBehaviour {
+    public KeyCodeScriptableObject currentKeyCode;
+    private Text uiText;
+
+    void Start() {
+        uiText = gameObject.GetComponent<Text>();
+    }
+    void Update() {
+        // Change "Alpha7" to "7"
+        string label = currentKeyCode.keyCode.ToString();
+        if (currentKeyCode.keyCode == KeyCode.Alpha7) {
+            label = "7";
+        }
+
+        uiText.text = label;
+    }
+}
