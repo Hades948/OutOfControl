b9409f7e2bf8676f11e1adc08499c0d69cdf42b5
Hades948 <hades948@gmail.com>
2020-07-24 11:19:27 -0400

    Update credits scene Moved music manager to title screen

--- Out Of Control/Assets/Scripts/CreditsController.cs
+++ Out Of Control/Assets/Scripts/CreditsController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CreditsController : MonoBehaviour {
     public float Speed;
+    private BlackFadeController BlackFadeControllerScript;
+
+    void Start() {
+        BlackFadeControllerScript = GameObject.Find("Black Fade").GetComponent<BlackFadeController>();
+    }
 
     void Update() {
         transform.position = new Vector3(transform.position.x, transform.position.y + Speed * Time.deltaTime, transform.position.z);
+    }
+
+    void OnBecameInvisible() {
+        BlackFadeControllerScript.FadeToScene("Title Screen");
     }
 }
---
+++ Out Of Control/Assets/Scripts/MusicManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManagerController : MonoBehaviour {
+    private static bool IsRunning = false;
+    
+    void Start() {
+        if (IsRunning) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+            IsRunning = true;
+        }
+    }
+}
